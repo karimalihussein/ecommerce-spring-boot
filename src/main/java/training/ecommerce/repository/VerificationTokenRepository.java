a414a4f47950824e2ce2c579fbeb9862315ffe06
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-11-15 23:09:33 +0200

    Add VerificationTokenRepository for email verification

---
+++ src/main/java/training/ecommerce/repository/VerificationTokenRepository.java
@@ -0,0 +1,15 @@
+package training.ecommerce.repository;
+
+
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
+
+import training.ecommerce.model.User;
+import training.ecommerce.model.VerificationToken;
+import java.util.Optional;
+
+@Repository
+public interface VerificationTokenRepository extends ListCrudRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
+    void deleteByUser(User user);
+}
