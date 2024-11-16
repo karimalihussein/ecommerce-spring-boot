234bfd937edb7846ece7f24f007434150b42a378
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-11-16 20:09:51 +0200

    Add application.properties and data.sql for configuring H2 database and inserting test data

---
+++ src/test/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users (email, first_name, last_name, password, username, email_verified)
+values ('user1@gmail.com', 'User', 'user', '$2a$10$bYAcfToJhWZIKwUAFWlHJe0Xucg7D7Xxf/zRJyIIPbM8P.t9ZVrFK', 'user1', true),
+       ('user2@gmail.com', 'User2', 'user2', '$2a$10$bYAcfToJhWZIKwUAFWlHJe0Xucg7D7Xxf/zRJyIIPbM8P.t9ZVrFK', 'user2', false);
