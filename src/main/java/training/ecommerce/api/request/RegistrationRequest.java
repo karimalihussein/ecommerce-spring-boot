2c41a02c4563b4c65203a035ac0bba41a3bb7f44
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-11-16 20:09:22 +0200

    Add validation annotations to RegistrationRequest fields

--- src/main/java/training/ecommerce/api/request/RegistrationRequest.java
+++ src/main/java/training/ecommerce/api/request/RegistrationRequest.java
@@ -2,6 +2,7 @@ package training.ecommerce.api.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -17,28 +18,46 @@ import lombok.extern.slf4j.Slf4j;
 public class RegistrationRequest {
 
     @NotBlank(message = "Username is required")
+    @NotNull(message = "Username is required")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
 
     @NotBlank(message = "Email is required")
+    @NotNull(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
 
     @NotBlank(message = "Password is required")
+    @NotNull(message = "Password is required")
     @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$", message = "Password must be at least 8 characters long, and contain at least one letter and one number")
     private String password;
 
     @NotBlank(message = "Confirm Password is required")
+    @NotNull(message = "Confirm Password is required")
     private String confirmPassword;
 
     @NotBlank(message = "First name is required")
+    @NotNull(message = "First name is required")
     @Size(max = 50, message = "First name must not exceed 50 characters")
     private String firstName;
 
     @NotBlank(message = "Last name is required")
+    @NotNull(message = "Last name is required")
     @Size(max = 50, message = "Last name must not exceed 50 characters")
     private String lastName;
+
+    public RegistrationRequest() {
+    }
+
+    public RegistrationRequest(String username, String email, String password, String confirmPassword, String firstName, String lastName) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     public String getUsername() {
         log.info("Accessing username: {}", username);
